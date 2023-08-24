9f3e62f724a1e14b7fa478a9bccc7163f2b5718f
whyred <112580881+ksalx@users.noreply.github.com>
2023-08-24 11:41:25 +0000

    Update save_result.py
    rename text

--- functions/save_result.py
+++ functions/save_result.py
@@ -6,12 +6,12 @@ def save_with_balance(wallet_info: tuple, wallets: list[dict]):
         for wallet in wallets:
             if wallet_info[0] == wallet['address']:
                 with open('With Balance.txt', 'a', encoding='utf-8') as file:
-                    file.writelines(f'Адрес : {wallet_info[0]}\n'
-                                    f'Ключ : {wallet["privkey"]}\n'
-                                    f'Пароль : {wallet["passphare"]}\n'
-                                    f'Транзакции : {wallet_info[1]["n_tx"]}\n'
-                                    f'Всего получено : {Decimal(wallet_info[1]["total_received"] / 1e8) :.8f}\n'
-                                    f'Баланс : {Decimal(wallet_info[1]["final_balance"] / 1e8) :.8f}\n\n\n'
+                    file.writelines(f'Address : {wallet_info[0]}\n'
+                                    f'Public Key : {wallet["privkey"]}\n'
+                                    f'Passphrase : {wallet["passphare"]}\n'
+                                    f'Transactions : {wallet_info[1]["n_tx"]}\n'
+                                    f'Total received : {Decimal(wallet_info[1]["total_received"] / 1e8) :.8f}\n'
+                                    f'Balance : {Decimal(wallet_info[1]["final_balance"] / 1e8) :.8f}\n\n\n'
                                     )
 
 
@@ -20,10 +20,10 @@ def save_with_transaction(wallet_info: tuple, wallets: list[dict]):
         for wallet in wallets:
             if wallet_info[0] == wallet['address']:
                 with open('With Transactions.txt', 'a', encoding='utf-8') as file:
-                    file.writelines(f'Адрес : {wallet_info[0]}\n'
-                                    f'Ключ : {wallet["privkey"]}\n'
-                                    f'Пароль : {wallet["passphare"]}\n'
-                                    f'Транзакции : {wallet_info[1]["n_tx"]}\n'
-                                    f'Всего получено : {Decimal(wallet_info[1]["total_received"] / 1e8) :.8f}\n'
-                                    f'Баланс : {Decimal(wallet_info[1]["final_balance"] / 1e8) :.8f}\n\n\n'
+                    file.writelines(f'Address : {wallet_info[0]}\n'
+                                    f'Public Key : {wallet["privkey"]}\n'
+                                    f'Passphrase : {wallet["passphare"]}\n'
+                                    f'Transactions : {wallet_info[1]["n_tx"]}\n'
+                                    f'Total received : {Decimal(wallet_info[1]["total_received"] / 1e8) :.8f}\n'
+                                    f'Balance : {Decimal(wallet_info[1]["final_balance"] / 1e8) :.8f}\n\n\n'
                                     )
